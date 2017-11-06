e5b31202ab5659480d2f6729ffa855e86227e55d
Vadim Sharai <ziberman.v.s@gmail.com>
2017-11-06 15:08:44 +0200

    Was added Category model and migration

--- database/migrations/2017_11_06_130741_create_categories_table.php
+++ database/migrations/2017_11_06_130741_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTestsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateTestsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tests', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('category');
-            $table->json('items');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateTestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tests');
+        Schema::dropIfExists('categories');
     }
 }
